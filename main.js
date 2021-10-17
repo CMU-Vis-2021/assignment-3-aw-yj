ff96196c70a9513f779ace7f8a239bff8ac766ab
Anthony <awu3@andrew.cmu.edu>
2021-10-17 12:39:19 -0400

    displaytest

--- main.js
+++ main.js
@@ -3,26 +3,116 @@ import vegaEmbed from "vega-embed";
 
 d3.select("#d3-div").append("p").text("hello from D3");
 
-vegaEmbed("#vega-div", {
-  $schema: "https://vega.github.io/schema/vega-lite/v5.json",
-  description: "A simple bar chart with embedded data.",
-  data: {
-    values: [
-      { Date: "A", Price: 28 },
-      { Date: "B", Price: 55 },
-      { Date: "C", Price: 43 },
-      { Date: "D", Price: 91 },
-      { Date: "E", Price: 81 },
-      { Date: "F", Price: 53 },
-      { Date: "G", Price: 19 },
-      { Date: "H", Price: 87 },
-      { Date: "I", Price: 52 },
-    ],
-  },
-  mark: "bar",
-  encoding: {
-    x: { field: "Date", type: "nominal", axis: { labelAngle: 0 } },
-    y: { field: "Price", type: "quantitative" },
-  },
+
+// set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 30, left: 50},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data
+d3.csv("https://raw.githubusercontent.com/CMU-Vis-2021/assignment-3-aw-yj/main/dataset/TeslaAll.csv", function(data) {
+
+  // List of groups (here I have one group per column)
+  var allGroup = d3.map(data, function(d){return(d.Type)}).keys()
+
+  // add the options to the button
+  d3.select("#selectButton")
+    .selectAll('myOptions')
+    .data(allGroup)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; }) // corresponding value returned by the button
+
+  // add the x Axis
+  var x = d3.scaleLinear()
+    .domain([0, 75])
+    .range([0, width]);
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // add the y Axis
+  var y = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, 0.04]);
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+  // Compute kernel density estimation for the first group called Setosa
+  var kde = kernelDensityEstimator(kernelEpanechnikov(2), x.ticks(200))
+  var density =  kde( data
+    .filter(function(d){ return d.Type == "Open"})
+    .map(function(d){  return +d.value; })
+  )
+
+  // Plot the area
+  var curve = svg
+    .append('g')
+    .append("path")
+      .attr("class", "mypath")
+      .datum(density)
+      .attr("fill", "#69b3a2")
+      .attr("opacity", ".8")
+      .attr("stroke", "#000")
+      .attr("stroke-width", 1)
+      .attr("stroke-linejoin", "round")
+      .attr("d",  d3.line()
+        .curve(d3.curveBasis)
+          .x(function(d) { return x(d[0]); })
+          .y(function(d) { return y(d[1]); })
+      );
+
+  // A function that update the chart when slider is moved?
+  function updateChart(selectedGroup) {
+    // recompute density estimation
+    kde = kernelDensityEstimator(kernelEpanechnikov(0.5), x.ticks(25))
+    var density =  kde( data
+      .filter(function(d){ return d.Type == selectedGroup})
+      .map(function(d){  return +d.value; })
+    )
+
+    // update the chart
+    curve
+      .datum(density)
+      .transition()
+      .duration(1000)
+      .attr("d",  d3.line()
+        .curve(d3.curveBasis)
+          .x(function(d) { return x(d[0]); })
+          .y(function(d) { return y(d[1]); })
+      );
+  }
+
+  // Listen to the slider?
+  d3.select("#selectButton").on("change", function(d){
+    selectedGroup = this.value
+    updateChart(selectedGroup)
+  })
+
 });
+
+
+// Function to compute density
+function kernelDensityEstimator(kernel, X) {
+  return function(V) {
+    return X.map(function(x) {
+      return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+    });
+  };
+}
+function kernelEpanechnikov(k) {
+  return function(v) {
+    return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+  };
+}
 
